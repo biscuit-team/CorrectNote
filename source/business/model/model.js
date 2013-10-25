1bba4963310d0fb3da543838ccb850c16d9a38a2
gongshw <gongshw1992@gmail.com>
2013-10-25 08:45:54 +0800

    model common api

---
+++ source/business/model/model.js
@@ -0,0 +1,59 @@
+/**
+ * Created by gongshw on 13-10-24.
+ */
+
+
+var model = (function (self) {
+	"use strict";
+
+	/**
+	 * @constructor
+	 */
+
+	/**
+	 * @param {mix} type
+	 * @param {*} value
+	 * @param {*} throwable
+	 * @returns {*}
+	 */
+	self.setValue = function (type, value) {
+		if ((typeof type === 'function')) {
+			if (value.constructor === type) {
+				return value;
+			} else {
+				throw 'need a ' + type.name;
+			}
+
+		} else if ((typeof type === 'string')) {
+			if (typeof value === type) {
+				return value;
+			} else {
+				throw 'need a ' + type;
+			}
+		} else {
+			throw 'first argument must be a Function or string';
+		}
+	}
+
+	self.extend = function (o, p) {
+
+		for (var index in p) {
+			console.log(index);
+			o[index] = p[index];
+			var getter = p.__lookupGetter__(index);
+			if (typeof getter === 'function') {
+				o.__defineGetter__(index,getter);
+			}
+
+			var setter = p.__lookupSetter__(index);
+			if (typeof setter === 'function') {
+				o.__defineSetter__(index,setter);
+			}
+
+		}
+		return o;
+	}
+
+	return self;
+
+})(model || {});
--- source/business/model/question.js
+++ source/business/model/question.js
@@ -1,14 +1,46 @@
 /**
  * Created by gongshw on 13-10-23.
  */
-var model = (function () {
+
+var model = (function (model, Content, Element) {
 	"use strict"
+
+	/**
+	 * @constructor
+	 */
 	function Question() {
+		this._tags = new Array();
+		this._q = new Content();
+		this._a = new Content;
+	}
+
+	Question.prototype = {
+
+		get body() {
+			return this._q;
+		},
+		set body(body) {
+			this._q = model.setValue(Content,body)
+		},
+
+		/**
+		 * @returns {Array}
+		 */
+		get tags() {
+			return this._tags;
+		},
+		/**
+		 * @param {Array} tags
+		 */
+		set tags(tags) {
+			this._tags = tags;
+		},
+
 
 	}
 
-	return {
-		Question: Question
-	};
+	model.Question = Question;
 
-})(model||{})
+	return model;
+
+})(model || {}, model.Content, model.Element);
