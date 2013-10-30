16453fd47531ae624c6db350446a7b2537560c60
shuhaozhang <zsh10@software.nju.edu.cn>
2013-10-30 13:44:44 +0800

    some test

--- source/controller/index.js
+++ source/controller/index.js
@@ -8,10 +8,13 @@
     var _wait_time = 1000;
 
     function _go_main(){
-
+		location.href = 'course.html';
     }
 
     window.onload = function () {
         setTimeout(_go_main, _wait_time);
     }
 })();
+
+
+var http = require('http');
